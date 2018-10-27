e1fc2ccf0e8f603509572b4a1c7261da2427a3ab
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-27 18:29:34 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,15 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+#row = "   |   |   "
+def display_board
+  cell = "   "
+  divider = "|"
+  demarcator = ""
+  11.times { demarcator << "-" }
+  row = cell + divider + cell + divider + cell
+
+  puts row
+  puts demarcator
+  puts row
+  puts demarcator
+  puts row
+end
